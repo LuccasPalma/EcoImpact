8144e29f76359dd3e4ee5d4d1202200bb53ba230
Luccas <luccaspalma98@gmail.com>
2026-08-31 21:11:40 -0300

    Criacao Tela Qualidade

--- app/src/main/java/com/example/ecoimpact/MainActivity.kt
+++ app/src/main/java/com/example/ecoimpact/MainActivity.kt
@@ -1,29 +1,36 @@
 package com.example.ecoimpact
 
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Card
-import androidx.compose.ui.unit.dp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.ecoimpact.ui.theme.EcoImpactTheme
+import kotlinx.coroutines.delay
+import androidx.compose.foundation.clickable
+import androidx.compose.material3.Button
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -38,19 +45,68 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun TelaInicial() {
 
-    val resposta = remember {
-        mutableStateOf("Carregando...")
-    }
+    var mostrarSplash by remember { mutableStateOf(true) }
+    var telaAtual by remember { mutableStateOf("home") }
 
     LaunchedEffect(Unit) {
-        resposta.value = buscarQualidadeDoAr()
+        delay(2000)
+        mostrarSplash = false
     }
 
+    if (mostrarSplash) {
+        TelaSplash()
+    } else {
+        if (telaAtual == "home") {
+            TelaHome(
+                onAbrirQualidadeDoAr = {
+                    telaAtual = "qualidade"
+                }
+            )
+        } else {
+            TelaQualidadeDoAr(
+                onVoltar = {
+                    telaAtual = "home"
+                }
+            )
+        }
+    }
+}
+@Composable
+fun TelaSplash() {
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Text(
+            text = "🌱",
+            fontSize = 60.sp
+        )
+
+        Text(
+            text = "EcoImpact",
+            style = MaterialTheme.typography.headlineMedium,
+            fontWeight = FontWeight.Bold
+        )
+
+        Text(
+            text = "Impacto ambiental na palma da sua mão",
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
+}
+
+@Composable
+fun TelaHome(
+    onAbrirQualidadeDoAr: () -> Unit
+) {
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(
-                top = 40.dp,
+                top = 90.dp,
                 start = 20.dp,
                 end = 20.dp,
                 bottom = 20.dp
@@ -70,17 +126,16 @@ fun TelaInicial() {
             text = "Qualidade ambiental da sua região"
         )
 
-        Text(
-            text = resposta.value
-        )
-
         Spacer(modifier = Modifier.height(24.dp))
 
         Card(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp)
+                .clickable {
+                    onAbrirQualidadeDoAr()
+                }
         ) {
+
             Column(
                 modifier = Modifier.padding(24.dp)
             ) {
@@ -104,7 +159,7 @@ fun TelaInicial() {
                 )
 
                 Text(
-                    text = "17,4 µg/m³",
+                    text = "20,4 µg/m³",
                     style = MaterialTheme.typography.headlineSmall
                 )
 
@@ -115,7 +170,7 @@ fun TelaInicial() {
                 )
 
                 Text(
-                    text = "17,8 µg/m³",
+                    text = "20,9 µg/m³",
                     style = MaterialTheme.typography.headlineSmall
                 )
 
@@ -127,5 +182,101 @@ fun TelaInicial() {
                 )
             }
         }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Card(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+
+            Column(
+                modifier = Modifier.padding(24.dp)
+            ) {
+
+                Text(
+                    text = "🌱 Impacto ambiental",
+                    style = MaterialTheme.typography.titleLarge,
+                    fontWeight = FontWeight.Bold
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Text(
+                    text = "Acompanhe a qualidade do ar e descubra como suas escolhas podem contribuir para um ambiente melhor."
+                )
+            }
+        }
     }
 }
+    @Composable
+    fun TelaQualidadeDoAr(
+        onVoltar: () -> Unit
+    ) {
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(90.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Text(
+                text = "🌬️ Qualidade do ar",
+                style = MaterialTheme.typography.headlineMedium,
+                fontWeight = FontWeight.Bold
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                text = "📍 São Paulo"
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Card(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+
+                Column(
+                    modifier = Modifier.padding(24.dp)
+                ) {
+
+                    Text(
+                        text = "PM2.5"
+                    )
+
+                    Text(
+                        text = "20,4 µg/m³",
+                        style = MaterialTheme.typography.headlineSmall
+                    )
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    Text(
+                        text = "PM10"
+                    )
+
+                    Text(
+                        text = "20,9 µg/m³",
+                        style = MaterialTheme.typography.headlineSmall
+                    )
+
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    Text(
+                        text = "🟢 Qualidade: Boa",
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Button(
+                onClick = onVoltar
+            ) {
+                Text("← Voltar")
+            }
+        }
+    }
